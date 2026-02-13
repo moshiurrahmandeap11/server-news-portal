ceea47ef8b56ec301fe63a75671b32214ffbae59
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-02-14 00:49:54 +0600

    make a basic index.js using typeScript

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+// json parsing middleware
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from typeScript Universe");
+});
+app.listen(port, () => {
+  console.log(`news portal server running as http://localhost:${port}`);
+});
